41e2b63eae397f78382772480039b7765b57f407
moukh-tari <mmoukhtari57@gmail.com>
2023-06-15 01:01:52 +0100

    tp2

---
+++ src/main/java/org/sid/bankaccountservice/service/AccountService.java
@@ -0,0 +1,11 @@
+package org.sid.bankaccountservice.service;
+
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+
+public interface AccountService {
+     BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO) ;
+
+
+
+}
